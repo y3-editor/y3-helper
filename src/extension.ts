e8c6f41ca2805e68e422ec3232cec8f42b8c2b2f
最萌小汐 <sumneko@hotmail.com>
2023-11-01 16:31:04 +0800

    总之先找到地图路径

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,90 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+function registerCommandOfInitProject(context: vscode.ExtensionContext) {
+    async function searchMapPathInProject(folder: vscode.Uri, depth: number): Promise<vscode.Uri | undefined> {
+        // 递归搜索目录下的 `header.map` 文件所在目录
+        try {
+            let state = await vscode.workspace.fs.stat(vscode.Uri.joinPath(folder, 'header.map'));
+            if (state.type === vscode.FileType.File) {
+                return folder;
+            }
+        } catch (error) {
+            // ignore
+        }
+        if (depth <= 0) {
+            return undefined;
+        }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "y3-helper" is now active!');
+        let files = await vscode.workspace.fs.readDirectory(folder);
+        for (const file of files) {
+            if (file[1] === vscode.FileType.Directory) {
+                let mapFolder = await searchMapPathInProject(vscode.Uri.joinPath(folder, file[0]), depth - 1);
+                if (mapFolder) {
+                    return mapFolder;
+                }
+            }
+        }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('y3-helper.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from y3-helper!');
-	});
+        return undefined;
+    }
+    
+    async function searchProjectPath(): Promise<vscode.Uri | undefined> {
+        // 先直接搜索打开的工作目录
+        if (vscode.workspace.workspaceFolders) {
+            for (const folder of vscode.workspace.workspaceFolders) {
+                // 检查一下上一层目录
+                let parentFolder = vscode.Uri.joinPath(folder.uri, '..');
+                let mapFolder = await searchMapPathInProject(parentFolder, 0)
+                             || await searchMapPathInProject(folder.uri, 3);
+                if (mapFolder) {
+                    return mapFolder;
+                }
+            }
+        }
 
-	context.subscriptions.push(disposable);
+        // 如果没有，则询问用户
+        let selectedFolders = await vscode.window.showOpenDialog({
+            canSelectFiles: true,
+            canSelectFolders: false, // 竟然不能同时选择文件和文件夹
+            canSelectMany: false,
+            openLabel: '选择地图路径',
+            filters: {
+                '地图': ['map', 'project']
+            }
+        });
+
+        let selectedFolder = selectedFolders?.[0];
+        if (!selectedFolder) {
+            return undefined;
+        }
+
+        if ((await vscode.workspace.fs.stat(selectedFolder)).type === vscode.FileType.File) {
+            selectedFolder = vscode.Uri.joinPath(selectedFolder, '..');
+        };
+        let parentFolder = vscode.Uri.joinPath(selectedFolder, '..');
+        let mapFolder = await searchMapPathInProject(parentFolder, 0)
+                     || await searchMapPathInProject(selectedFolder, 3);
+        if (mapFolder) {
+            return mapFolder;
+        }
+
+        return undefined;
+    }
+
+    let disposable = vscode.commands.registerCommand('y3-helper.initProject', async () => {
+        let mapFolder = await searchProjectPath();
+        if (!mapFolder) {
+            vscode.window.showErrorMessage('未找到地图路径，请先用编辑器创建地图！');
+            return;
+        };
+        vscode.window.showInformationMessage(`地图路径：${mapFolder.fsPath}`);
+    });
+
+    context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+export function activate(context: vscode.ExtensionContext) {
+    registerCommandOfInitProject(context);
+}
+
 export function deactivate() {}
