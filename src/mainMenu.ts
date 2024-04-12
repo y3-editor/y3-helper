9c3f1d542d1cdaffd275b16f27a00cb789eb825b
最萌小汐 <sumneko@hotmail.com>
2024-04-12 16:45:24 +0800

    整理代码

--- src/mainMenu.ts
+++ src/mainMenu.ts
@@ -9,13 +9,13 @@ interface TreeNodeOptional {
     description?: typeof vscode.TreeItem.prototype.description;
     childs?: TreeNode[];
     update?: (node: TreeNode) => void | Thenable<void>;
-    hide?: boolean | ((node: TreeNode) => boolean | Promise<boolean>);
+    show?: boolean | ((node: TreeNode) => boolean | Promise<boolean>);
 }
 
 class TreeNode extends vscode.TreeItem {
     childs?: TreeNode[];
     parent?: TreeNode;
-    hide?: TreeNodeOptional["hide"];
+    show?: TreeNodeOptional["show"] = true;
     update?: (node: TreeNode) => void | Thenable<void>;
     constructor(label: string, optional?: TreeNodeOptional) {
         super(label, vscode.TreeItemCollapsibleState.None);
@@ -25,7 +25,7 @@ class TreeNode extends vscode.TreeItem {
             this.description = optional.description;
             this.childs = optional.childs;
             this.update = optional.update;
-            this.hide = optional.hide;
+            this.show = optional.show ?? true;
             this.collapsibleState = optional.collapsibleState;
         }
         this.updateChilds();
@@ -94,106 +94,126 @@ class ViewInNewVSCode extends TreeNode {
     }
 }
 
-let nodeReselectMapPath = new TreeNode('重新选择Y3地图路径', {
-    command: {
-        command: 'y3-helper.reloadEnv',
-        title: '重新选择Y3地图路径',
-    },
-    iconPath: new vscode.ThemeIcon('search'),
-});
-
-let nodeAction = new TreeNode('功能', {
-    iconPath: new vscode.ThemeIcon('beaker'),
-    collapsibleState: vscode.TreeItemCollapsibleState.Expanded,
+let mainNode = new TreeNode('主菜单', {
     childs: [
-        new TreeNode('初始化Y3库', {
-            command: {
-                command: 'y3-helper.initProject',
-                title: '初始化Y3库',
+        new TreeNode('功能', {
+            iconPath: new vscode.ThemeIcon('beaker'),
+            show: async () => {
+                await env.mapReady();
+                return env.scriptUri !== undefined;
             },
-            update: async (node) => {
-                node.iconPath = new vscode.ThemeIcon('cloud-download');
-                try {
-                    let stat = await vscode.workspace.fs.stat(vscode.Uri.joinPath(env.y3Uri!, '.git'));
-                    if (stat.type === vscode.FileType.Directory) {
-                        node.iconPath = new vscode.ThemeIcon('check');
+            collapsibleState: vscode.TreeItemCollapsibleState.Expanded,
+            childs: [
+                new TreeNode('初始化Y3库', {
+                    command: {
+                        command: 'y3-helper.initProject',
+                        title: '初始化Y3库',
+                    },
+                    update: async (node) => {
+                        node.iconPath = new vscode.ThemeIcon('cloud-download');
+                        try {
+                            let stat = await vscode.workspace.fs.stat(vscode.Uri.joinPath(env.y3Uri!, '.git'));
+                            if (stat.type === vscode.FileType.Directory) {
+                                node.iconPath = new vscode.ThemeIcon('check');
+                            }
+                        } catch {}
+                    },
+                    show: async () => {
+                        try {
+                            await vscode.workspace.fs.stat(vscode.Uri.joinPath(env.y3Uri!, '.git'));
+                            return false;
+                        } catch {
+                            return true;
+                        }
                     }
-                } catch {}
-            },
+                }),
+                new TreeNode('启动游戏', {
+                    command: {
+                        command: 'y3-helper.launchGame',
+                        title: '启动游戏',
+                    },
+                    iconPath: new vscode.ThemeIcon('play'),
+                }),
+                new TreeNode('启动游戏并附加调试器', {
+                    command: {
+                        command: 'y3-helper.launchGameAndAttach',
+                        title: '启动游戏并附加调试器',
+                    },
+                    iconPath: new vscode.ThemeIcon('run-all'),
+                    description: 'Shift + F5',
+                }),
+                new TreeNode('查看日志', {
+                    iconPath: new vscode.ThemeIcon('output'),
+                    show: () => {
+                        return env.scriptUri !== undefined;
+                    },
+                    update: async (node) => {
+                        if (env.scriptUri === undefined) {
+                            return;
+                        }
+                        node.command = {
+                            command: 'vscode.open',
+                            title: '查看日志',
+                            arguments: [vscode.Uri.joinPath(env.scriptUri!, 'log/lua_player01.log')]
+                        };
+                    },
+                }),
+            ]
         }),
-        new TreeNode('启动游戏', {
+
+        new TreeNode('环境', {
+            iconPath: new vscode.ThemeIcon('server-environment'),
+            show: async () => {
+                await env.mapReady();
+                return env.scriptUri !== undefined;
+            },
+            childs: [
+                new TreeNode('编辑器', {
+                    update: async (node) => {
+                        await env.editorReady(true);
+                        node.tooltip     = env.editorUri?.fsPath;
+                        node.iconPath    = env.editorUri ? new vscode.ThemeIcon('settings') : new vscode.ThemeIcon('error');
+                        node.description = env.editorUri ? env.editorUri.fsPath : '未找到编辑器';
+                        node.childs      = env.editorUri ? [
+                            new TreeNode('启动编辑器', {
+                                command: {
+                                    command: 'y3-helper.shell',
+                                    title: '启动编辑器',
+                                    arguments: [
+                                        'start',
+                                        env.editorUri.fsPath,
+                                    ]
+                                },
+                                iconPath: new vscode.ThemeIcon('play'),
+                            }),
+                            new ViewInExplorerNode(env.editorUri),
+                        ] : undefined;
+                    },
+                }),
+                new TreeNode('Lua脚本', {
+                    update: async (node) => {
+                        await env.mapReady(true);
+                        node.tooltip     = env.scriptUri?.fsPath;
+                        node.iconPath    = env.scriptUri ? new vscode.ThemeIcon('book') : new vscode.ThemeIcon('error');
+                        node.description = env.scriptUri ? env.scriptUri.fsPath : '未找到Lua脚本';
+                        node.childs      = env.scriptUri ? [
+                            new ViewInExplorerNode(env.scriptUri),
+                            new ViewInVSCode(env.scriptUri),
+                            new ViewInNewVSCode(env.scriptUri),
+                        ] : undefined;
+                    },
+                })
+            ],
+        }),
+
+        new TreeNode('重新选择Y3地图路径', {
             command: {
-                command: 'y3-helper.launchGame',
-                title: '启动游戏',
+                command: 'y3-helper.reloadEnv',
+                title: '重新选择Y3地图路径',
             },
-            iconPath: new vscode.ThemeIcon('play'),
-        }),
-        new TreeNode('启动游戏并附加调试器', {
-            command: {
-                command: 'y3-helper.launchGameAndAttach',
-                title: '启动游戏并附加调试器',
-            },
-            iconPath: new vscode.ThemeIcon('run-all'),
-            description: 'Shift + F5',
-        }),
-        new TreeNode('查看日志', {
-            iconPath: new vscode.ThemeIcon('output'),
-            hide: () => {
-                return env.scriptUri === undefined;
-            },
-            update: async (node) => {
-                if (env.scriptUri === undefined) {
-                    return;
-                }
-                node.command = {
-                    command: 'vscode.open',
-                    title: '查看日志',
-                    arguments: [vscode.Uri.joinPath(env.scriptUri!, 'log/lua_player01.log')]
-                };
-            },
+            iconPath: new vscode.ThemeIcon('search'),
         }),
     ]
-});
-
-let nodeEnv = new TreeNode('环境', {
-    iconPath: new vscode.ThemeIcon('server-environment'),
-    childs: [
-        new TreeNode('编辑器', {
-            update: async (node) => {
-                await env.editorReady(true);
-                node.tooltip     = env.editorUri?.fsPath;
-                node.iconPath    = env.editorUri ? new vscode.ThemeIcon('settings') : new vscode.ThemeIcon('error');
-                node.description = env.editorUri ? env.editorUri.fsPath : '未找到编辑器';
-                node.childs      = env.editorUri ? [
-                    new TreeNode('启动编辑器', {
-                        command: {
-                            command: 'y3-helper.shell',
-                            title: '启动编辑器',
-                            arguments: [
-                                'start',
-                                env.editorUri.fsPath,
-                            ]
-                        },
-                        iconPath: new vscode.ThemeIcon('play'),
-                    }),
-                    new ViewInExplorerNode(env.editorUri),
-                ] : undefined;
-            },
-        }),
-        new TreeNode('Lua脚本', {
-            update: async (node) => {
-                await env.mapReady(true);
-                node.tooltip     = env.scriptUri?.fsPath;
-                node.iconPath    = env.scriptUri ? new vscode.ThemeIcon('book') : new vscode.ThemeIcon('error');
-                node.description = env.scriptUri ? env.scriptUri.fsPath : '未找到Lua脚本';
-                node.childs      = env.scriptUri ? [
-                    new ViewInExplorerNode(env.scriptUri),
-                    new ViewInVSCode(env.scriptUri),
-                    new ViewInNewVSCode(env.scriptUri),
-                ] : undefined;
-            },
-        })
-    ],
 });
 
 class TreeProvider implements vscode.TreeDataProvider<TreeNode> {
@@ -201,28 +221,25 @@ class TreeProvider implements vscode.TreeDataProvider<TreeNode> {
     onDidChangeTreeData = this.refresh.event; 
 
     async getChildren(node?: TreeNode): Promise<TreeNode[] | undefined> {
-        if (!node) {
-            await env.mapReady();
-            if (!env.scriptUri) {
-                return [
-                    nodeReselectMapPath
-                ];
-            }
-            return [
-                nodeAction,
-                nodeEnv,
-            ];
+        node = node ?? mainNode;
+
+        if (node.childs === undefined) {
+            return undefined;
         }
 
-        let childs = node.childs?.filter(async (child) => {
-            if (child.hide === true) {
-                return false;
+        let childs = [];
+        for (const child of node.childs) {
+            if (child.show instanceof Function) {
+                let show = await child.show(child);
+                if (!show) {
+                    continue;
+                }
             }
-            if (child.hide instanceof Function) {
-                return await child.hide(child);
+            if (!child.show) {
+                continue;
             }
-            return true;
-        });
+            childs.push(child);
+        }
 
         if (childs?.length === 0) {
             return undefined;
@@ -290,5 +307,5 @@ export function reveal() {
     if (!mainMenu) {
         return;
     }
-    mainMenu.view.reveal(nodeAction, { focus: true, select: false, expand: true });
+    mainMenu.view.reveal(mainNode.childs![0], { focus: true, select: false, expand: true });
 }
