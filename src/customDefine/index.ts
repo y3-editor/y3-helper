d595d8c2557008f75ee1433b08b9dd88b19acb3b
ChNopx <389004294@qq.com>
2024-05-18 19:03:20 +0800

    添加物编mate生成

--- src/customDefine/index.ts
+++ src/customDefine/index.ts
@@ -1,9 +1,15 @@
 import { PlayerAttrs } from "./playerAttrs";
 import { UnitAttrs } from "./unitAttrs";
+import { EditorTables } from "./editorTables";
 import { Events } from "./event";
 
 export const define = {
     单位属性: new UnitAttrs,
     玩家属性: new PlayerAttrs,
     自定义事件: new Events,
+    单位类型: new EditorTables("editorunit"),
+    技能类型: new EditorTables("abilityall"),
+    物品类型: new EditorTables("editoritem"),
+    魔法效果: new EditorTables("modifierall"),
+    投射物: new EditorTables("projectileall"),
 };
