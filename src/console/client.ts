5cb3d72a60d8aebd9839f478657975fda13c3cd7
最萌小汐 <sumneko@hotmail.com>
2024-05-25 16:02:27 +0800

    暂存

--- src/console/client.ts
+++ src/console/client.ts
@@ -20,8 +20,8 @@ export function registerMethod(method: string, handler: Handler) {
     methods.set(method, handler);
 }
 
-const COLOR = {
-    NONE: '\x1b[0m',
+const CSI = {
+    RESET_COLOR: '\x1b[0m',
     BLACK: '\x1b[30m',
     RED: '\x1b[31m',
     GREEN: '\x1b[32m',
@@ -38,6 +38,19 @@ const COLOR = {
     BG_MAGENTA: '\x1b[45m',
     BG_CYAN: '\x1b[46m',
     BG_WHITE: '\x1b[47m',
+
+    CURSOR_UP: '\x1b[A',
+    CURSOR_DOWN: '\x1b[B',
+    CURSOR_FORWARD: '\x1b[C',
+    CURSOR_BACK: '\x1b[D',
+    CURSOR_NEXT_LINE: '\x1b[E',
+    CURSOR_PREV_LINE: '\x1b[F',
+    CURSOR_HORIZONTAL_ABSOLUTE: '\x1b[G',
+    CURSOR_HIDE: '\x1b[?25l',
+    CURSOR_SHOW: '\x1b[?25h',
+    CURSOR_REQUEST_POSITION: '\x1b[6n',
+
+    CLEAR_LINE: '\x1b[K',
 };
 
 class Pseudoterminal implements vscode.Pseudoterminal {
@@ -50,7 +63,7 @@ class Pseudoterminal implements vscode.Pseudoterminal {
     onDidWrite: vscode.Event<string>;
 
     open() {
-        this.write(`${COLOR.GREEN}>${COLOR.NONE}`);
+        this.newStart();
     };
 
     close() {};
@@ -58,71 +71,123 @@ class Pseudoterminal implements vscode.Pseudoterminal {
     private applyInput(data: string) {
     }
 
-    private inputData: string = '';
-    private cursorAt: number = 0;
+    private inputedData: string = '';
+    private curOffset: number = 0;
+    private headPos = [0, 0];
 
-    private refreshLine(fillNum: number) {
-        this.write('\r');
-        this.write(`${COLOR.GREEN}>${COLOR.NONE}`);
-        this.write(this.inputData);
-        for (let i = 0; i < fillNum * 3; i++) {
-            this.write(' \b');
+    private refreshLine(data: string) {
+        this.moveCursor(0);
+        this.write(CSI.CLEAR_LINE);
+        this.inputedData = data;
+        this.write(data);
+        this.moveCursor(this.curOffset);
+    }
+
+    handleInput(data: string): void {
+        if (data === '\r') { // 回车键
+            this.applyInput(this.inputedData);
+            this.inputedData = '';
+            this.curOffset = 0;
+            this.write('\r\n');
+            this.newStart();
+            return;
+        } else if (data === '\x7F') { // 删除前一个文字
+            if (this.curOffset > 0) {
+                this.refreshLine(this.inputedData.slice(0, this.curOffset - 1) + this.inputedData.slice(this.curOffset));
+                this.curOffset -= 1;
+                this.moveCursor(this.curOffset);
+            }
+            return;
+        } else if (data === '\x1B') { // ESC键
+            this.refreshLine('');
+            this.curOffset = 0;
+            this.moveCursor(this.curOffset);
+            return;
+        } else if (data === '\x03') { // Ctrl+C
+            vscode.env.clipboard.writeText(this.inputedData);
+            return;
+        } else if (data === '\x16') { // Ctrl+V
+            vscode.env.clipboard.readText().then((text) => {
+                this.handleCommonInput(text);
+            });
+            return;
+        }
+
+        if (data.startsWith('\x1B[')) {
+            this.handleControl(data);
+            return;
+        }
+
+        this.handleCommonInput(data);
+    }
+
+    private handleControl(data: string) {
+        let func = data.slice(-1);
+        let args = data.slice(2, -1).split(';').map((arg) => parseInt(arg));
+
+        switch (func) {
+            case '~': // 一些特殊键
+                if (args[0] === 3) { // 删除键
+                    this.refreshLine(this.inputedData.slice(0, this.curOffset) + this.inputedData.slice(this.curOffset + 1));
+                } else if (args[0] === 1) { // Home键
+                    this.curOffset = 0;
+                    this.moveCursor(this.curOffset);
+                } else if (args[0] === 4) { // End键
+                    this.curOffset = this.inputedData.length;
+                    this.moveCursor(this.curOffset);
+                }
+                break;
+            case 'A': // 上箭头
+                break;
+            case 'B': // 下箭头
+                break;
+            case 'C': // 右箭头
+                if (this.curOffset < this.inputedData.length) {
+                    this.curOffset += 1;
+                    this.moveCursor(this.curOffset);
+                }
+                break;
+            case 'D': // 左箭头
+                if (this.curOffset > 0) {
+                    this.curOffset -= 1;
+                    this.moveCursor(this.curOffset);
+                }
+                break;
+            case 'R': // 光标位置回应
+                this.responseCursorPos(args[0], args[1]);
+                break;
+            default:
+                break;
         }
     }
 
-    handleInput(data: string) {
-        switch (data) {
-            case '\r': // 回车键
-                this.write(`\r\n${COLOR.GREEN}>${COLOR.NONE}`);
-                this.applyInput(this.inputData);
-                this.inputData = '';
-                break;
-            case '\x7F': // 删除前一个文字
-                if (this.cursorAt > 0) {
-                    this.inputData = this.inputData.slice(0, this.cursorAt - 1) + this.inputData.slice(this.cursorAt);
-                    this.cursorAt--;
-                    this.refreshLine(1);
-                }
-                break;
-            case '\x1B': // ESC键
-                let length = this.inputData.length;
-                this.inputData = '';
-                this.cursorAt = 0;
-                this.refreshLine(length);
-                break;
-            case '\x03': // Ctrl+C
-                // 复制当前输入的所有内容
-                vscode.env.clipboard.writeText(this.inputData);
-                break;
-            case '\x16': // Ctrl+V
-                // 粘贴剪贴板内容
-                vscode.env.clipboard.readText().then((text) => {
-                    this.write(text);
-                    this.inputData += text;
-                    this.cursorAt += text.length;
-                });
-                break;
-            case '\x1B[1~': // Home键
-                break;
-            case '\x1B[3~': // 删除键（删除后一个字符）
-                break;
-            case '\x1B[4~': // End键
-                break;
-            case '\x1B[A': // 上箭头
-                break;
-            case '\x1B[B': // 下箭头
-                break;
-            case '\x1B[C': // 右箭头
-                break;
-            case '\x1B[D': // 左箭头
-                break;
-            default:
-                if (data.match(/^[\x20-\x7E\u4E00-\u9FA5]*$/)) {
-                    // 只有输入全部为可见字符（包括unicode字符）时才显示
-                    this.inputData += data;
-                    this.write(data);
-                };
-        }
+    private handleCommonInput(data: string) {
+        data = data.replace(/[^\x20-\x7E\u4E00-\u9FA5]/g, ' ');
+        this.curOffset += data.length;
+        this.refreshLine(this.inputedData.slice(0, this.curOffset) + data + this.inputedData.slice(this.curOffset));
+    }
+
+    private moveCursor(offset: number) {
+        let row = this.headPos[0];
+        let col = this.headPos[1] + offset;
+        this.write(`\x1B[${row};${col}H`);
+    }
+
+    private newStart() {
+        this.write(`${CSI.GREEN}>${CSI.RESET_COLOR}`);
+        this.requestCursorPos((row, col) => {
+            this.headPos = [row, col];
+        });
+    }
+
+    private cursorRequests: ((row: number, col: number) => void)[] = [];
+    private requestCursorPos(callback: (row: number, col: number) => void) {
+        this.cursorRequests.push(callback);
+        this.write(CSI.CURSOR_REQUEST_POSITION);
+    }
+    private responseCursorPos(row: number, col: number) {
+        let callback = this.cursorRequests.shift();
+        callback?.(row, col);
     }
 
     write(data: string) {
