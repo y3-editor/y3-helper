16b4ee5fb19c8dbdc51fb7a2988af9d90a5a9f12
最萌小汐 <sumneko@hotmail.com>
2024-07-10 21:00:29 +0800

    生成定义文件

--- 生成定义.bat
+++ 生成定义.bat
@@ -1,4 +1,2 @@
-tsc -d --emitDeclarationOnly --outDir ./tmp/types
-dts-bundle --name y3-helper --main tmp\types\y3-helper.d.ts --out binded.d.ts
-xcopy /y /e .\tmp\types\binded.d.ts .\template\excel\rule\y3-helper.d.ts
+dts-bundle --name y3-helper --main tmp\types\y3-helper.d.ts --out ..\..\template\excel\rule\y3-helper.d.ts
 pause
