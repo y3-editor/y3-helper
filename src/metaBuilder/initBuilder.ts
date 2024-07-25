6e60496f74579d43018a31e613a0ff70307c2f60
最萌小汐 <sumneko@hotmail.com>
2024-07-25 19:06:47 +0800

    整理代码

--- src/metaBuilder/initBuilder.ts
+++ src/metaBuilder/initBuilder.ts
@@ -3,8 +3,6 @@ import { BaseBuilder } from './baseBuilder';
 import { env } from '../env';
 import { PlayerAttrs } from './playerAttrs';
 import { CustomEvents } from './customEvents';
-import { EditorTablesBuilder } from "./editorTables";
-import { define } from '../customDefine';
 import { UI } from './ui';
 
 let path = 'y3-helper/meta';
@@ -41,21 +39,11 @@ export function init() {
     let unitAttrs = new UnitAttrs(path + '/unitAttrs.lua');
     let playerAttrs = new PlayerAttrs(path + '/playerAttrs.lua');
     let customEvents = new CustomEvents(path + '/customEvents.lua');
-    // let editorunit = new EditorTablesBuilder(path + '/unitKeys.lua', 'unitKeys', define.单位类型);
-    // let abilityall = new EditorTablesBuilder(path + '/abilityKeys.lua', 'abilityKeys', define.技能类型);
-    // let editoritem = new EditorTablesBuilder(path + '/itemKeys.lua', 'itemKeys', define.物品类型);
-    // let modifierall = new EditorTablesBuilder(path + '/buffKeys.lua', 'buffKeys', define.魔法效果类型);
-    // let projectileall = new EditorTablesBuilder(path + '/projectileKeys.lua', 'projectileKeys', define.投射物类型);
     let ui = new UI(path + '/ui.lua');
 
     initBuilder.addFile(unitAttrs);
     initBuilder.addFile(playerAttrs);
     initBuilder.addFile(customEvents);
-    // initBuilder.addFile(editorunit);
-    // initBuilder.addFile(abilityall);
-    // initBuilder.addFile(editoritem);
-    // initBuilder.addFile(modifierall);
-    // initBuilder.addFile(projectileall);
     initBuilder.addFile(ui);
 
     unitAttrs.onDidChange(() => {
